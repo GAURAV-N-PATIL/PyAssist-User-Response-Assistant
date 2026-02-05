7df3c5d41ed611db8f33827771381898b75f4840
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-02-05 22:34:55 +0530

    Intent recognition done

--- src/PyAssist.py
+++ src/PyAssist.py
@@ -6,6 +6,17 @@ User_input = input("Enter a command: ")
 User_input = User_input.strip()
 User_input = User_input.lower()
 
+# intent recognition only (no output response)
+Greeting_keywords = ["hello", "hi", "hey", "greetings","hey there","good morning","good afternoon","good evening","how are you","what's up","how's it going","hello there","hi PyAssist"]
+Exit_keywords = ["exit", "quit", "goodbye", "bye","see you later","take care","farewell","catch you later","talk to you later","have a nice day","have a good day"]
+Identity_keywords = ["who are you", "what is your name", "what do you do", "what can you do","tell me about yourself","what's your name","what's your purpose","what's your function","what's your role"]
+intent = "Unknown"
+if User_input in Greeting_keywords:
+    intent = "Greeting"
+elif User_input in Exit_keywords:
+    intent = "Exit"
+elif User_input in Identity_keywords:
+    intent = "Identity"
 
 # function to run the main program
 def main():
